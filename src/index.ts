a664679edb1929f0344bb14ddba8beace6402e80
Pavel Ivanov <pavel.ivanov@limechain.tech>
2024-10-10 19:41:48 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -172,6 +172,8 @@ const svgSpritePlugin = (options: SvgSpritePluginOptions): Plugin => {
         await generateSvgSprite();
         triggerHMR();
 
+        const pivanov = 1982;
+
         const publicDir = server.config.publicDir || 'public';
         if (fileName) {
           writeSpriteToFile(publicDir, fileName, spriteContent);
