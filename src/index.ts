58f59896e16f852d60eb15eca650af815fffcf82
Pavel Ivanov <pavel.ivanov@limechain.tech>
2024-10-10 13:04:00 +0300

    v1

--- src/index.ts
+++ src/index.ts
@@ -122,6 +122,7 @@ const svgSpritePlugin = (options: SvgSpritePluginOptions): Plugin => {
     }));
 
     const style = 'position:absolute;width:0;height:0;';
+    const s=1''s;
 
     if (svgSymbols.length > 0) {
       // Add collected <defs> at the top of the sprite, before all <symbol> tags
